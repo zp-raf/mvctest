6c2e3e5a7adedbd735139b708e6832208c4a8361
federicop91 <federicop91@gmail.com>
2014-12-07 23:21:16 -0300

    Se quita la opcion insert del grid de factura detalle. Se cambia la descripcion de la deuda.

--- v1/model/frmgeneradeudadatamodule.pas
+++ v1/model/frmgeneradeudadatamodule.pas
@@ -147,8 +147,9 @@ begin
     DescOriginal := DataSet.FieldByName('DESCRIPCION').AsString;
   // aca ponemos la descripcion de la deuda
   DataSet.FieldByName('DESCRIPCION').AsString :=
-    Personas.Persona.FieldByName('NOMBRECOMPLETO').AsString + ' - ' +
-    DescOriginal + SEPARADOR + Arancel.ArancelNOMBRE.AsString + ESPACIO +
+   // Personas.Persona.FieldByName('NOMBRECOMPLETO').AsString + ' - ' +
+    DescOriginal + //SEPARADOR +
+    Arancel.ArancelNOMBRE.AsString + ESPACIO +
     DataSet.FieldByName('CUOTA_NRO').AsString + SEPARADOR_CUOTA +
     DataSet.FieldByName('CANTIDAD_CUOTAS').AsString + SEPARADOR;
   // ponemos el monto de la deuda
